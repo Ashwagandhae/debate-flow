daa1b875e8d465eb51e3ab0e042b1ba8df09e760
Ashwagandhae <julianlianbauer@gmail.com>
2023-07-07 14:03:36 -0700

    added favicon

--- .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
+++ .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
@@ -7,10 +7,10 @@ function __memo(fn) {
 return {
 	appDir: "_app",
 	appPath: "_app",
-	assets: new Set(["favicon.png","robots.txt"]),
+	assets: new Set(["favicon.png","favicon_old.png","robots.txt"]),
 	mimeTypes: {".png":"image/png",".txt":"text/plain"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.e1145375.js","app":"_app/immutable/entry/app.6a478c9a.js","imports":["_app/immutable/entry/start.e1145375.js","_app/immutable/chunks/scheduler.2a49f677.js","_app/immutable/chunks/singletons.8ec54e57.js","_app/immutable/chunks/index.7f4ed86a.js","_app/immutable/entry/app.6a478c9a.js","_app/immutable/chunks/scheduler.2a49f677.js","_app/immutable/chunks/index.8a1ca1b8.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.ac907441.js","app":"_app/immutable/entry/app.df439ded.js","imports":["_app/immutable/entry/start.ac907441.js","_app/immutable/chunks/scheduler.2a49f677.js","_app/immutable/chunks/singletons.91436465.js","_app/immutable/chunks/index.7f4ed86a.js","_app/immutable/entry/app.df439ded.js","_app/immutable/chunks/scheduler.2a49f677.js","_app/immutable/chunks/index.8a1ca1b8.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			__memo(() => import('../output/server/nodes/0.js')),
 			__memo(() => import('../output/server/nodes/1.js'))
