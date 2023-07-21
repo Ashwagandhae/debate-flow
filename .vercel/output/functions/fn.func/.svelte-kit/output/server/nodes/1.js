56e6d95eb8939cf722f24f28ed81d0346fb598da
Nick K <nick@arguflow.gg>
2023-07-21 17:09:05 -0400

    bugfix: add .vercel folder to gitignore

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.4f6ca039.js","_app/immutable/chunks/scheduler.d2fddee9.js","_app/immutable/chunks/index.f8f1350c.js","_app/immutable/chunks/singletons.245bc642.js","_app/immutable/chunks/index.71cbf4be.js"];
-export const stylesheets = [];
-export const fonts = [];
